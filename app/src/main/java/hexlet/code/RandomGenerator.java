0537f389070b528afcb9b6c456d3447ca3caa50a
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-02-09 19:03:28 +0300

    Create RandomGenerator class
    This class is used to generat a random number using the linear congruence method formula

--- app/src/main/java/hexlet/code/RandomGenerator.java
+++ app/src/main/java/hexlet/code/RandomGenerator.java
@@ -1,4 +1,32 @@
 package hexlet.code;
 
 public class RandomGenerator {
+
+    // Используется линейный конгруэнтный метод для вычисления псевдослучайного числа
+    // Каждое новое число вычисляется по формуле
+    // Xn+1 = (a * Xn + c) % m,
+    // где X0 (seed) - начальное значение.
+
+    private int seed;
+    private int currentNumber;
+
+    RandomGenerator(int seed) {
+        this.seed = seed;
+        this.currentNumber = seed;
+    }
+
+    public int generateNextNumber() {
+
+        var a = 7;
+        var c = 35;
+        var m = 285;
+
+        var randomNumber = (a * currentNumber + c) % m;
+        currentNumber = randomNumber;
+        return randomNumber;
+    }
+
+    public void reset() {
+        currentNumber = seed;
+    }
 }
