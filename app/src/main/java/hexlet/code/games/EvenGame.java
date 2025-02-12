76c2a6f96eb76f29de8b3893a4003653101a36da
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-02-12 23:18:54 +0300

    refactor: Improve EvenGame logic

--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -1,71 +1,40 @@
 package hexlet.code.games;
 
-import hexlet.code.Greeting;
-import hexlet.code.User;
-
+import hexlet.code.Engine;
 import java.util.Scanner;
-
-import static hexlet.code.games.RandomGenerator.generateRandomNumber;
-// import hexlet.code.evenGame.RandomGenerator;
 
 public class EvenGame {
 
-    public static void playEvenGame() {
-
-        var user = new User();
-        Greeting.greet(user);
-        var name = user.getName();
-
-        var scanner = new Scanner(System.in);
-
-        // Количество раундов в в одной игре
-        var roundsCount = 3;
-
-        // Закончена ли игра. Игра считается законченной, если успешно прошло roundsCount раундов
-        var isGameOver = false;
-
-        // Счетчик раундов
-        var currentRoundsCount = 0;
-
-        // Максимальное случайное число
-        var limit = 275;
-
-
+    public static void play(Scanner scanner) {
+        // Приветствуем пользователя
+        Engine.sendWelcomeMessage();
+        var userName = Engine.askUserName(scanner);
+        Engine.sendGreetingByNameMessage(userName);
+        // Задаем количество раундов
+        var roundsCount = Engine.defaultRoundsCount;
+        // Задаем максимальное случайное число
+        var limit = 1000;
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-
-        while (!isGameOver) {
-
+        for (int i = 0; i < roundsCount; i++) {
             // Получаем случайное число
-            var newNumber = generateRandomNumber(limit);
+            var newNumber = Engine.generateRandomNumber(limit);
             // Узнаем правильный ответ
             var correctAnswer = isEven(newNumber) ? "yes" : "no";
-            System.out.println("Question: " + newNumber);
+            // Задаем вопрос пользователю
+            var question = Integer.toString(newNumber);
+            Engine.askUser(question);
             // Получаем ответ пользователя
-            var userAnswer = scanner.nextLine();
-            // Проверяем, совпадает ли ответ пользователя с правильным ответов
-            var isCorrectAnswer = userAnswer.equals(correctAnswer);
-
-            if (isCorrectAnswer) {
-                // Если пользователь ответил верно,
-                // то сообщаем пользователю об этом
-                user.tellCorrect();
-                // Увеличиваем счетчик пройденных раундов
-                currentRoundsCount++;
-                // Проверяем, является ли текущий раунд финальным
-                isGameOver = currentRoundsCount == roundsCount;
-                // Если пройденный раунд был финальным,
-                // поздравляем пользователя с успешно пройденной игрой!
-                if (isGameOver) {
-                    user.congratulate();
-                }
-
-            } else {
-
-                user.tellIncorrect(userAnswer, correctAnswer);
-                isGameOver = true;
-
+            var userAnswer = Engine.getUserAnswer(scanner);
+            // Проверяем ответ пользователя
+            var isCorrectAnswer = Engine.checkAnswer(userAnswer, correctAnswer);
+            // Выводим результат в зависимости от того, как ответил пользователь
+            Engine.printResult(userAnswer, correctAnswer, isCorrectAnswer, userName);
+            // Проверяем, является ли текущий раунд финальным
+            // Если раунд финальный - поздравляем пользователя
+            var isFinalRound = i == roundsCount - 1;
+            if (isCorrectAnswer && isFinalRound) {
+                Engine.congratulate(userName);
             }
-
         }
 
     }
@@ -74,6 +43,7 @@ public class EvenGame {
         // Проверяем, является ли число четным
         // Число является четным если делиться на 2 без остатка
         return number % 2 == 0;
+
     }
 
 }
