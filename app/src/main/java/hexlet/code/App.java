e4dc3c097462eff252f088a326b31507f9540661
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-02-15 22:37:17 +0300

    feat: Add ProgressionGame to game selection in App.main
    Integrated the ProgressionGame into the game selection menu within the
    App.main method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.games.CalcGame;
 import hexlet.code.games.EvenGame;
 import hexlet.code.games.GcdGame;
+import hexlet.code.games.ProgressionGame;
 
 import java.util.Scanner;
 
@@ -15,6 +16,7 @@ public class App {
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
 
@@ -36,6 +38,9 @@ public class App {
                 break;
             case 4:
                 GcdGame.play(scanner);
+                break;
+            case 5:
+                ProgressionGame.play(scanner);
                 break;
             default:
                 System.out.println("Incorrect choice");
