7266c9321b62325aed7a8061549c32e5252353ec
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-02-16 20:20:22 +0300

    feat: Implement PrimeGame class for Prime game

---
+++ app/src/main/java/hexlet/code/games/PrimeGame.java
@@ -0,0 +1,52 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Scanner;
+
+public class PrimeGame {
+
+    public static void play(Scanner scanner) {
+        Engine.sendWelcomeMessage();
+        var userName = Engine.askUserName(scanner);
+        Engine.sendGreetingByNameMessage(userName);
+        var roundsCount = Engine.defaultRoundsCount;
+        var task = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        Engine.printTask(task);
+        for (int i = 0; i < roundsCount; i++) {
+            var newNumber = getNewNumber();
+            var correctAnswer = isPrimeNumber(newNumber) ? "yes" : "no";
+            var question = Integer.toString(newNumber);
+            Engine.askUser(question);
+            var userAnswer = Engine.getUserAnswer(scanner);
+            var isCorrectAnswer = Engine.checkAnswer(userAnswer, correctAnswer);
+            Engine.printResult(userAnswer, correctAnswer, isCorrectAnswer, userName);
+            var isFinalRound = i == roundsCount - 1;
+            if (!isCorrectAnswer) {
+                return;
+            }
+            if (isFinalRound) {
+                Engine.congratulate(userName);
+            }
+        }
+    }
+
+    public static int getNewNumber() {
+        var limit = 53;
+        return Engine.generateRandomNumber(53);
+    }
+
+
+    public static boolean isPrimeNumber(int number) {
+        var isPrime = true;
+        var rootOfNumber = (int) Math.sqrt((double) number);
+        for (int i = 2; i <= rootOfNumber; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+            }
+        }
+
+        return isPrime;
+    }
+
+}
