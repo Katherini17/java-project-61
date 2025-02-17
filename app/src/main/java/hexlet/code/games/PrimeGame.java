6c18f3255e64432d2e6fee54c0525c7523c8de98
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-02-18 00:10:50 +0300

    refactor: Remove game logic from PrimeGame

--- app/src/main/java/hexlet/code/games/PrimeGame.java
+++ app/src/main/java/hexlet/code/games/PrimeGame.java
@@ -5,41 +5,35 @@ import hexlet.code.Engine;
 import java.util.Scanner;
 
 public class PrimeGame {
-    public static final Integer MAX_RANDOM_NUMBER = 53;
 
-    public static void play(Scanner scanner) {
-        Engine.sendWelcomeMessage();
-        var userName = Engine.askUserName(scanner);
-        Engine.sendGreetingByNameMessage(userName);
-        int roundsCount = Engine.DEFAULT_ROUNDS_COUNT;
-        var task = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-        Engine.printTask(task);
-        for (int i = 0; i < roundsCount; i++) {
-            var newNumber = Engine.generateRandomNumber(MAX_RANDOM_NUMBER);
-            var correctAnswer = isPrimeNumber(newNumber) ? "yes" : "no";
-            var question = Integer.toString(newNumber);
-            Engine.askUser(question);
-            var userAnswer = Engine.getUserAnswer(scanner);
-            var isCorrectAnswer = Engine.checkAnswer(userAnswer, correctAnswer);
-            Engine.printResult(userAnswer, correctAnswer, isCorrectAnswer, userName);
-            var isFinalRound = i == roundsCount - 1;
-            if (!isCorrectAnswer) {
-                return;
-            }
-            if (isFinalRound) {
-                Engine.congratulate(userName);
-            }
+    public static final Integer MAX_RANDOM_NUMBER = 53;
+    public static final String TASK = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+
+    public static void playPrimeGame(Scanner scanner) {
+        var questions = new String[Engine.DEFAULT_ROUNDS_COUNT];
+        var correctAnswers = new String[Engine.DEFAULT_ROUNDS_COUNT];
+
+        for (int i = 0; i < Engine.DEFAULT_ROUNDS_COUNT; i++) {
+            var randomNumber = Engine.generateRandomNumber(MAX_RANDOM_NUMBER);
+
+            var correctAnswer = isPrimeNumber(randomNumber) ? "yes" : "no";
+            correctAnswers[i] = correctAnswer;
+
+            var question = Integer.toString(randomNumber);
+            questions[i] = question;
         }
+
+        Engine.play(scanner, TASK, questions, correctAnswers);
     }
 
     public static boolean isPrimeNumber(int number) {
-
+        // 1 не является простым числом
         if (number == 1) {
             return false;
         }
-
+        // Проверяем, что число не делиться без остатка
+        // на делители от 2 до корня из числа
         var rootOfNumber = (int) Math.sqrt(number);
-
         for (int i = 2; i <= rootOfNumber; i++) {
             if (number % i == 0) {
                 return false;
